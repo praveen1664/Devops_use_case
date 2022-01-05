10815799bd5434d18a3ea2b1c77299b8109b3d73
Praveen <36715149+praveen1664@users.noreply.github.com>
2022-01-05 21:59:02 +0530

    Update shell_script.sh

--- shell_script.sh
+++ shell_script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-IMAGE_NAME="ecosia"
+IMAGE_NAME="ecosiatree"
 #uncoment the below line if minikube is not running
 #minikube start
 #eval $(minikube docker-env) 
 eval $(minikube -p minikube docker-env)
 docker build -t $IMAGE_NAME:latest .
-kubectl apply -f manifest.yml
-sleep 1m
-kubectl get pods -o wide
+minikube kubectl -- create -f manifest.yml
+#minikube kubectl -- get pods -o wide
+
  
